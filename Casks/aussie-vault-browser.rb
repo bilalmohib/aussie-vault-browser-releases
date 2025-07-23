f92f3e831602133751a17b2ba22b8f442c054b13
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2025-07-23 18:51:58 +0500

    fix: Update cask URL to match actual DMG filename

--- Casks/aussie-vault-browser.rb
+++ Casks/aussie-vault-browser.rb
@@ -2,7 +2,7 @@ cask "aussie-vault-browser" do
   version "1.0.2"
   sha256 :no_check
 
-  url "https://github.com/bilalmohib/aussie-vault-browser-releases/releases/download/v#{version}/Aussie%20Vault%20Browser-#{version}-arm64.dmg"
+  url "https://github.com/bilalmohib/aussie-vault-browser-releases/releases/download/v#{version}/AussieVaultBrowser-#{version}.dmg"
   name "Aussie Vault Browser"
   desc "Secure remote browser with VPN capabilities and 1Password integration"
   homepage "https://github.com/bilalmohib/aussie-vault-browser-releases"
